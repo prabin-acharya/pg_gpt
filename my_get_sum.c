e7ab45966761c114219d99d571690c4d36c1ab6e
Prabin Acharya <acharyaprabin101@gmail.com>
2023-02-03 22:24:56 +0545

    api request to OpenAPI!!!. lfg!!

--- my_get_sum.c
+++ my_get_sum.c
@@ -7,7 +7,9 @@
 PG_MODULE_MAGIC;
 #endif
 
-#define API_URL "https://jsonplaceholder.typicode.com/posts/5"
+#define API_URL "https://api.openai.com/v1/completions"
+#define API_HEADER1 "Content-Type: application/json"
+#define API_HEADER2 "Authorization: Bearer  secret-api-token-here"
 
 static size_t write_callback(void *contents, size_t size, size_t nmemb, void *userp)
 {
@@ -23,16 +25,25 @@ Datum my_get_sum(PG_FUNCTION_ARGS)
 {
     CURL *curl;
     CURLcode res;
+    struct curl_slist *headers = NULL;
+
     char response[1024] = {0};
+    char data[] = "{\"model\": \"code-davinci-002\", \"prompt\": \"Say this is a test\", \"temperature\": 0, \"max_tokens\": 7}";
 
     curl = curl_easy_init();
     if (curl)
     {
+        headers = curl_slist_append(headers, API_HEADER1);
+        headers = curl_slist_append(headers, API_HEADER2);
+
         curl_easy_setopt(curl, CURLOPT_URL, API_URL);
+        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data);
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, response);
         res = curl_easy_perform(curl);
         curl_easy_cleanup(curl);
+        curl_slist_free_all(headers);
     }
 
     PG_RETURN_TEXT_P(cstring_to_text(response));
