50198fa632c6ae01ed1301dde944290bb4e082f7
Prabin Acharya <acharyaprabin101@gmail.com>
2023-02-05 16:52:51 +0545

    fix

--- gpt_query_to_sql--0.0.1.sql
+++ gpt_query_to_sql--0.0.1.sql
@@ -4,3 +4,30 @@
 CREATE OR REPLACE FUNCTION gpt_query_to_sql(text) RETURNS  text
 AS '$libdir/gpt_query_to_sql'
 LANGUAGE C IMMUTABLE STRICT;
+
+CREATE OR REPLACE FUNCTION concat_numbers(a INTEGER, b INTEGER)
+RETURNS TEXT AS $$
+BEGIN
+    RETURN CAST(a AS TEXT) || CAST(b AS TEXT);
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION execute_concat_numbers(a INTEGER, b INTEGER)
+RETURNS TEXT AS $$
+BEGIN
+    RETURN concat_numbers(a, b);
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION execute_gpt_sql(a text)
+RETURNS refcursor AS $$
+DECLARE
+    result refcursor;
+BEGIN
+    -- EXECUTE gpt_query_to_sql(a);
+    -- EXECUTE 'SELECT * FROM actor LIMIT 10';
+     OPEN result FOR EXECUTE 'SELECT * FROM actor LIMIT 10';
+    RETURN result;
+END;
+$$ LANGUAGE plpgsql;
+
--- gpt_query_to_sql.c
+++ gpt_query_to_sql.c
@@ -1,18 +1,12 @@
 
 #include <postgres.h>
-// #include "json.h"
 #include <fmgr.h>
 #include <utils/builtins.h>
 #include <curl/curl.h>
 #include <catalog/pg_namespace.h>
 #include <catalog/pg_class.h>
-#include <catalog/pg_attribute.h>
-#include <access/heapam.h>
 #include <executor/spi.h>
-#include <access/htup_details.h>
-#include <access/tupdesc.h>
 #include <utils/typcache.h>
-#include "funcapi.h"
 
 #ifdef PG_MODULE_MAGIC
 PG_MODULE_MAGIC;
@@ -20,7 +14,7 @@ PG_MODULE_MAGIC;
 
 #define API_URL "https://api.openai.com/v1/completions"
 #define API_HEADER1 "Content-Type: application/json"
-#define API_HEADER2 "Authorization: Bearer my-secret"
+#define API_HEADER2 "Authorization: Bearer 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"
 
 static size_t
 write_callback(void *contents, size_t size, size_t nmemb, void *userp)
@@ -31,27 +25,18 @@ write_callback(void *contents, size_t size, size_t nmemb, void *userp)
     return realsize;
 }
 
-char *replace_newline(char *src)
+char *replace_newline(char *str)
 {
-    int i, j, len;
-    char *dst = NULL;
-    len = strlen(src);
-    dst = malloc((len + 1) * sizeof(char));
-
-    for (i = 0, j = 0; i < len; i++)
+    char *dst = str;
+    for (const char *src = str; *src != '\0'; ++src)
     {
-        if (src[i] == '\n')
+        if (*src != '\n')
         {
-            dst[j++] = ' ';
-        }
-        else
-        {
-            dst[j++] = src[i];
+            *dst++ = *src;
         }
     }
-    dst[j] = '\0';
-
-    return dst;
+    *dst = '\0';
+    return str;
 }
 
 const char *get_text(const char *json)
@@ -69,10 +54,10 @@ const char *get_text(const char *json)
         return "text not found";
     }
     int text_len = text_end - text_start;
-    char *text = (char *)malloc(text_len + 1);
+    char *text = (char *)malloc(4096 * sizeof(char));
     strncpy(text, text_start, text_len);
     text[text_len] = '\0';
-    return text;
+    return replace_newline(text);
 }
 
 PG_FUNCTION_INFO_V1(gpt_query_to_sql);
@@ -80,23 +65,22 @@ PG_FUNCTION_INFO_V1(gpt_query_to_sql);
 Datum gpt_query_to_sql(PG_FUNCTION_ARGS)
 {
 
-    text *natural_query = PG_GETARG_TEXT_P(0);
-    char *mattt = text_to_cstring(natural_query);
+    text *natural_query_text = PG_GETARG_TEXT_P(0);
+    char *natural_query = text_to_cstring(natural_query_text);
 
     CURL *curl;
     CURLcode res;
 
     struct curl_slist *headers = NULL;
 
-    char response[2048] = {0};
+    char response[4096] = {0};
 
     int ret;
     int i;
     TupleDesc tupdesc;
     SPITupleTable *tuptable;
     HeapTuple tuple;
-    char *result = NULL;
-    char *requestp = NULL;
+    char *db_schema = NULL;
     char *json_result = NULL;
 
     ret = SPI_connect();
@@ -105,7 +89,7 @@ Datum gpt_query_to_sql(PG_FUNCTION_ARGS)
         ereport(ERROR, (errcode(ERRCODE_INTERNAL_ERROR), errmsg("SPI_connect failed")));
     }
 
-    ret = SPI_exec("SELECT CONCAT(table_name, '(', column_names, ')') as formatted_table FROM (SELECT table_name, string_agg(column_name, ', ') as column_names FROM information_schema.columns WHERE table_schema = 'public' GROUP BY table_name ORDER BY table_name) as subquery;", 0);
+    ret = SPI_exec("SELECT CONCAT(table_name, '(', column_names, ')') as formatted_table FROM (SELECT table_name, string_agg(column_name, ', ') as column_names FROM information_schema.columns WHERE table_schema = 'bookings' GROUP BY table_name ORDER BY table_name) as subquery;", 0);
     if (ret != SPI_OK_SELECT)
     {
         ereport(ERROR, (errcode(ERRCODE_INTERNAL_ERROR), errmsg("SPI_exec failed")));
@@ -113,19 +97,16 @@ Datum gpt_query_to_sql(PG_FUNCTION_ARGS)
 
     tupdesc = SPI_tuptable->tupdesc;
     tuptable = SPI_tuptable;
-    result = palloc(2048 * sizeof(char));
-    strcpy(result, "");
-    requestp = palloc(2048 * sizeof(char));
-    strcpy(requestp, "");
+    db_schema = palloc(4096 * sizeof(char));
+    strcpy(db_schema, "");
 
     for (i = 0; i < SPI_processed; i++)
     {
         tuple = tuptable->vals[i];
-        snprintf(result, 2048, "%s%s", result, replace_newline(SPI_getvalue(tuple, tupdesc, 1)));
+        snprintf(db_schema, 2048, "%s%s\\n", db_schema, replace_newline(SPI_getvalue(tuple, tupdesc, 1)));
     }
-
-    // =======================================================
-
+    headers = curl_slist_append(headers, API_HEADER1);
+    headers = curl_slist_append(headers, API_HEADER2);
     curl = curl_easy_init();
     if (!curl)
     {
@@ -134,26 +115,15 @@ Datum gpt_query_to_sql(PG_FUNCTION_ARGS)
 
     char *api_url2 = "http://localhost:3000/api/hello";
 
-    // char datarr[] = "{\r\n  \"model\": \"code-davinci-002\",\r\n  \"prompt\": \"### Postgres SQL tables, with their properties:\\n#\\n#";
-    // char dataMiddle[] = result;
-    // char datarrend[] = "\\n #\\n## #Query : A query to list the names of the departments which employed more than 10 employees in the last 3 months\\nSQL :\",\r\n  \"temperature\": 0,\r\n  \"max_tokens\": 150,\r\n  \"top_p\": 1.0,\r\n  \"frequency_penalty\": 0.0,\r\n  \"presence_penalty\": 0.0,\r\n  \"stop\": [\"#\", \";\"]\r\n}";
+    json_result = palloc(4096 * sizeof(char));
+    snprintf(json_result, 4096, "{\r\n  \"model\": \"text-davinci-003\",\r\n \"prompt\": \"### Here are Postgres SQL tables with their properties:\\n %s . Now, I will write a  query to databse in natural language and you should translate them to appropriate SQL command according to the Postgres Tables for that. Also make sure the table exists in the given data and if it does not exist match the closest. Also write SQL command in a single line.  Query:%s\\n SQL:\"}", replace_newline(db_schema), natural_query);
 
-    // strcat(datarr, result);
-    // strcat(datarr, datarrend);
     headers = curl_slist_append(headers, API_HEADER1);
     headers = curl_slist_append(headers, API_HEADER2);
-    char rit[5] = "pran";
 
-    json_result = palloc(2048 * sizeof(char));
-    snprintf(json_result, 2048, "{\r\n  \"model\": \"text-davinci-003\",\r\n \"prompt\": \"### Here are Postgres SQL tables with their properties:\\n# %s \\n. Now, I will write a  query to databse in natural language and you should translate them to appropriate SQL command for that.  Query:%s \\n SQL:\"}", replace_newline(result), mattt);
-
-    // snprintf(requestp, 2048, "model=code-davinci-002&prompt=### Postgres SQL tables, with their properties:\n#\n%s#\n#\n### Query: A query to list the names of the departments which employed more than 10 employees in the last 3 months\nSQL:&temperature=0", result);
-    // snprintf(requestp, 2048, "{\r\n  \"model\": \"code-davinci-002\",\r\n  \"prompt\": \"### Postgres SQL tables, with their properties:\\n#\\n# %s\\n#\\n### Query: A query to list the names of the departments which employed more than 10 employees in the last 3 months\\nSQL:\",\r\n  \"temperature\": 0,\r\n  \"max_tokens\": 150,\r\n  \"top_p\": 1.0,\r\n  \"frequency_penalty\": 0.0,\r\n  \"presence_penalty\": 0.0,\r\n  \"stop\": [\"#\", \";\"]\r\n}", result);
-    // snprintf(requestp, 2048, "{\r\n  \"model\": \"code-davinci-002\",\r\n\"prompt\":\"asdff%sfds\"}", rit);
     curl_easy_setopt(curl, CURLOPT_URL, API_URL);
     curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
 
-    // curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "{\r\n  \"model\": \"code-davinci-002\"}");
     curl_easy_setopt(curl, CURLOPT_POSTFIELDS, json_result);
     curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
     curl_easy_setopt(curl, CURLOPT_WRITEDATA, response);
@@ -168,14 +138,5 @@ Datum gpt_query_to_sql(PG_FUNCTION_ARGS)
 
     SPI_finish();
 
-    // struct json_object *parsed_json = json_tokener_parse(response);
-    // struct json_object *result2, *text;
-    // bool success;
-
-    // success = json_object_object_get_ex(parsed_json, "result", &result2);
-    // success = json_object_array_get_idx(result2, 0, &text);
-    // success = json_object_object_get_ex(text, "text", &text);
-
-    PG_RETURN_TEXT_P(cstring_to_text(get_text(response)));
-    // PG_RETURN_TEXT_P(cstring_to_text(mattt));
+    PG_RETURN_TEXT_P(cstring_to_text(replace_newline(get_text(response))));
 }
