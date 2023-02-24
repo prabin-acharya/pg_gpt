47b91c08168db11b3870a79ab535ef9eafbdfeff
Prabin Acharya <acharyaprabin101@gmail.com>
2023-02-25 00:26:10 +0545

    gpt_explain

--- pg_gpt.c
+++ pg_gpt.c
@@ -125,8 +125,6 @@ Datum gpt_query(PG_FUNCTION_ARGS)
         ereport(ERROR, (errcode(ERRCODE_INTERNAL_ERROR), errmsg("curl_easy_init failed")));
     }
 
-    char *api_url2 = "http://localhost:3000/api/hello";
-
     headers = curl_slist_append(headers, API_HEADER1);
     headers = curl_slist_append(headers, API_HEADER2);
 
@@ -155,7 +153,80 @@ Datum gpt_query(PG_FUNCTION_ARGS)
 
 Datum gpt_explain(PG_FUNCTION_ARGS)
 {
-    PG_RETURN_TEXT_P(cstring_to_text("gpt_explain"));
+
+    text *natural_query_text = PG_GETARG_TEXT_P(0);
+    char *natural_query = text_to_cstring(natural_query_text);
+
+    CURL *curl;
+    CURLcode res;
+
+    struct curl_slist *headers = NULL;
+
+    char response[4096] = {0};
+
+    int ret;
+    int i;
+    TupleDesc tupdesc;
+    SPITupleTable *tuptable;
+    HeapTuple tuple;
+    char *db_schema = NULL;
+    char *json_result = NULL;
+
+    ret = SPI_connect();
+    if (ret != SPI_OK_CONNECT)
+    {
+        ereport(ERROR, (errcode(ERRCODE_INTERNAL_ERROR), errmsg("SPI_connect failed")));
+    }
+
+    ret = SPI_exec("SELECT CONCAT(table_name, '(', column_names, ')') as formatted_table FROM (SELECT table_name, string_agg(column_name, ', ') as column_names FROM information_schema.columns WHERE table_schema = 'bookings' GROUP BY table_name ORDER BY table_name) as subquery;", 0);
+    if (ret != SPI_OK_SELECT)
+    {
+        ereport(ERROR, (errcode(ERRCODE_INTERNAL_ERROR), errmsg("SPI_exec failed")));
+    }
+
+    tupdesc = SPI_tuptable->tupdesc;
+    tuptable = SPI_tuptable;
+    db_schema = palloc(4096 * sizeof(char));
+    strcpy(db_schema, "");
+
+    for (i = 0; i < SPI_processed; i++)
+    {
+        tuple = tuptable->vals[i];
+        snprintf(db_schema, 4096, "%s%s\\n", db_schema, replace_newline(SPI_getvalue(tuple, tupdesc, 1)));
+    }
+
+    // =======================================================
+
+    curl = curl_easy_init();
+    if (!curl)
+    {
+        ereport(ERROR, (errcode(ERRCODE_INTERNAL_ERROR), errmsg("curl_easy_init failed")));
+    }
+
+    headers = curl_slist_append(headers, API_HEADER1);
+    headers = curl_slist_append(headers, API_HEADER2);
+
+    json_result = palloc(4096 * sizeof(char));
+    snprintf(json_result, 4096, "{\r\n  \"model\": \"text-davinci-003\",\r\n \"prompt\": \"### Here are Postgres SQL tables with their properties:\\n %s . Now, For a SQL Query expain the query in natural language clearly in a single line.  Query:%s\\n Explanation:\"}", replace_newline(db_schema), natural_query);
+
+    curl_easy_setopt(curl, CURLOPT_URL, API_URL);
+    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, json_result);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, response);
+
+    res = curl_easy_perform(curl);
+    if (res != CURLE_OK)
+    {
+        ereport(ERROR, (errcode(ERRCODE_INTERNAL_ERROR), errmsg("curl_easy_perform failed: %s", curl_easy_strerror(res))));
+    }
+
+    curl_easy_cleanup(curl);
+
+    SPI_finish();
+
+    PG_RETURN_TEXT_P(cstring_to_text(get_text(response)));
 }
 
 Datum gpt_plan(PG_FUNCTION_ARGS)
